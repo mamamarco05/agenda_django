d643a296d30274cc64e0ab609a15278d88f26863
unknown <dangelo182225@gmail.com>
2020-09-28 06:19:05 -0300

    Criado tela de listagem de eventos

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from core import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/agenda/')),
     path('admin/', admin.site.urls),
+    path('eventos/<titulo_evento>', views.evento),
+    path('agenda/', views.lista_eventos)
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+
+def evento(requests, titulo_evento):
+    objeto = Evento.objects.get(titulo=titulo_evento)
+    return HttpResponse('<p>{}</p><p>{}</p>'.format(objeto.titulo,objeto.data_evento))
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
